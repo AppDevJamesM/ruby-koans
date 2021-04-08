30efa057c024b94cac6dbdb23dd1ea74da079f6b
AppDevJam <james.m@chicagobooth.edu>
2021-04-08 21:20:16 +0000

    Completed triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  sides = [a, b, c].sort
+  raise TriangleError, "Sides must be greater than zero" unless sides[0] > 0 
+  raise TriangleError, "Impossible triangle" if sides[0] + sides[1] <= sides[2]
+
   if a == b && b == c
     return :equilateral
   elsif a == b or a == c or b == c
@@ -29,3 +33,9 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+#def triangle(a, b, c)  
+  #sides = [a, b, c].sort
+  #raise TriangleError, "Sides must be greater than zero" unless sides[0] > 0 
+  #raise TriangleError, "Impossible triangle" if sides[0] + sides[1] <= sides[2]
+#end
+
