57d331f19d841f1b294621aac96c45620161fd7d
AppDevJam <james.m@chicagobooth.edu>
2021-04-12 01:33:22 +0000

    Completed inheritance

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class ::DiceSet
+attr_accessor :values
+  def roll(rolls)
+    @values = (0...rolls).map { 1 + rand(6) }
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
